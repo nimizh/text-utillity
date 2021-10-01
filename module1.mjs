b041583a984f5c20b7eb0fb8929c7818f5d2df07
Nimish Wadhawan <nimish.wadhawan.nw@gmail.com>
2021-10-01 19:50:24 +0530

    Updates

--- module1.mjs
+++
@@ -1,5 +0,0 @@
-import uz, { a, b, c } from "./module2.mjs";
-console.log(uz);
-console.log(a);
-console.log(b);
-console.log(c);
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "one direction";
-const b = " black bird";
-const c = "green day";
-const d = " little pink";
-
-export default d;
-export { a };
-export { b };
-export { c };
